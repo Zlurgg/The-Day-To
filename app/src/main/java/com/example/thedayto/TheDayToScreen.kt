a0c22abdebdeede80efa505c4faa09b1bd4a8fad
Zlurgg <joseph.brightman@gmail.com>
2023-07-14 15:47:33 +0100

    Added mood buttons, submit happy or sad to pass value to mood screen Need to work out if I need a view model of Mood to pass assign value to

--- app/src/main/java/com/example/thedayto/TheDayToScreen.kt
+++ app/src/main/java/com/example/thedayto/TheDayToScreen.kt
@@ -1,19 +1,28 @@
 package com.example.thedayto
 
 import androidx.annotation.StringRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 
+/** screen names **/
 enum class TheDayToScreen(@StringRes val title: Int) {
     Home(title = R.string.home),
     Mood(title = R.string.mood),
@@ -26,30 +35,52 @@ fun TheDayToApp() {
         navController = navController,
         startDestination = TheDayToScreen.Home.name,
         modifier = Modifier.padding(24.dp)
-
     ) {
         composable(TheDayToScreen.Home.name) {
             HomeScreen(
-                onNextButtonClicked = {
+                onSubmitMoodButtonClicked = {
                     navController.navigate(TheDayToScreen.Mood.name)
+
                 },
             )
         }
         composable(TheDayToScreen.Mood.name) {
-            MoodScreen()
+            MoodScreen("")
         }
     }
 }
 
+class MoodViewModel {
+
+}
+
 @Composable
 fun HomeScreen(
-    onNextButtonClicked: () -> Unit,
+    onSubmitMoodButtonClicked: (String) -> Unit,
     modifier: Modifier = Modifier
 ) {
-    Button(
-        onClick = onNextButtonClicked
+    Row(
+        horizontalArrangement = Arrangement.SpaceBetween,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp)
     ) {
-        Text("Mood")
+        Button(
+            onClick = { onSubmitMoodButtonClicked("Happy") },
+        ) {
+            Image(
+                painter = painterResource(R.drawable.happy_face),
+                contentDescription = "Happy face"
+            )
+        }
+        Button(
+            onClick = { onSubmitMoodButtonClicked("Sad") },
+        ) {
+            Image(
+                painter = painterResource(R.drawable.sad_face),
+                contentDescription = "Sad face"
+            )
+        }
     }
 }
 
@@ -57,33 +88,22 @@ fun HomeScreen(
 @Composable
 fun HomeScreenPreview() {
     HomeScreen(
-        onNextButtonClicked = {},
-        modifier = Modifier.fillMaxSize().padding(24.dp)
+        onSubmitMoodButtonClicked = {},
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(24.dp)
     )
 }
 
 @Composable
-fun MoodScreen(
-) {
-    Text("Mood")
+fun MoodScreen(mood: String) {
+    Text("Mood = $mood")
 }
 
 
 @Preview(showBackground = true)
 @Composable
 fun MoodScreenPreview() {
-    MoodScreen()
+    MoodScreen("")
 }
-
-
-/*@Composable
-fun CalenderScreen() {
-    Text("Calender")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun CalenderScreenPreview() {
-    CalenderScreen()
-}*/
 
