0e5841538848b0f5a2f247308328a5df343d0c34
Zlurgg <joseph.brightman@gmail.com>
2023-07-14 16:56:10 +0100

    refactored out calender to different class, added a return button to page

--- app/src/main/java/com/example/thedayto/TheDayToScreen.kt
+++ app/src/main/java/com/example/thedayto/TheDayToScreen.kt
@@ -3,17 +3,20 @@ package com.example.thedayto
 import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -24,11 +27,11 @@ enum class TheDayToScreen(@StringRes val title: Int) {
     Calender(title = R.string.calender),
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TheDayToApp() {
     val navController = rememberNavController()
     var mood = ""
-
     NavHost(
         navController = navController,
         startDestination = TheDayToScreen.Home.name,
@@ -39,11 +42,15 @@ fun TheDayToApp() {
                 onSubmitMoodButtonClicked = {
                     navController.navigate(TheDayToScreen.Calender.name)
                     mood = it
-                },
+                }
             )
         }
         composable(TheDayToScreen.Calender.name) {
-            CalenderScreen(mood)
+            CalenderScreen(
+                mood = mood,
+                onReturnButtonClicked = {
+                backToHome(navController)
+            })
         }
     }
 }
@@ -53,29 +60,35 @@ fun HomeScreen(
     onSubmitMoodButtonClicked: (String) -> Unit,
     modifier: Modifier = Modifier
 ) {
-    Row(
-        horizontalArrangement = Arrangement.SpaceBetween,
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(8.dp)
-    ) {
-        Button(
-            onClick = { onSubmitMoodButtonClicked("happy_face") },
+    Column() {
+        Text(
+            modifier = Modifier.fillMaxWidth(),
+            text = "How're you feeling today?")
+        Row(
+            horizontalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp)
         ) {
-            Image(
-                painter = painterResource(R.drawable.happy_face),
-                contentDescription = "happy mood"
-            )
-        }
-        Button(
-            onClick = { onSubmitMoodButtonClicked("sad_face") },
-        ) {
-            Image(
-                painter = painterResource(R.drawable.sad_face),
-                contentDescription = "sad mood"
-            )
+            Button(
+                onClick = { onSubmitMoodButtonClicked("happy_face") },
+            ) {
+                Image(
+                    painter = painterResource(R.drawable.happy_face),
+                    contentDescription = "happy mood"
+                )
+            }
+            Button(
+                onClick = { onSubmitMoodButtonClicked("sad_face") },
+            ) {
+                Image(
+                    painter = painterResource(R.drawable.sad_face),
+                    contentDescription = "sad mood"
+                )
+            }
         }
     }
+
 }
 
 @Preview(showBackground = true)
@@ -87,23 +100,8 @@ fun HomeScreenPreview() {
             .fillMaxSize())
 }
 
-@Composable
-fun CalenderScreen(mood: String) {
-    val id = if (mood == "sad_face") {
-        R.drawable.sad_face
-    } else {
-        R.drawable.happy_face
-    }
-    Image(
-        painter = painterResource(id = id),
-        contentDescription = "displayed mood"
-    )
+private fun backToHome(
+    navController: NavHostController
+) {
+    navController.popBackStack(TheDayToScreen.Home.name, inclusive = false)
 }
-
-
-@Preview(showBackground = true)
-@Composable
-fun CalenderScreenPreview() {
-    CalenderScreen("")
-}
-
