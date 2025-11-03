75a90995f1fd483e64d8d6430d1c7833298d3b83
Zlurgg <joseph.brightman@gmail.com>
2025-11-03 16:13:33 +0000

    Test Infrastructure Created:
      1. FakePreferencesRepository.kt - In-memory test double for preferences
      2. FakeNotificationRepository.kt - In-memory test double with error simulation support
    
      3. FakeEntryRepository.kt - In-memory test double for entries
      4. FakeOverviewUseCases.kt - Factory function to create OverviewUseCases with fakes
    
      Test Coverage:
    
      Created 11 comprehensive test cases in OverviewViewModelTest.kt:

--- app/src/test/java/uk/co/zlurgg/thedayto/journal/ui/overview/OverviewViewModelTest.kt
+++ app/src/test/java/uk/co/zlurgg/thedayto/journal/ui/overview/OverviewViewModelTest.kt
@@ -4,7 +4,6 @@ import app.cash.turbine.test
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.StandardTestDispatcher
-import kotlinx.coroutines.test.advanceUntilIdle
 import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.runTest
 import kotlinx.coroutines.test.setMain
