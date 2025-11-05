b647de37a59a04f7fc34a8c806c4c089554d6146
Zlurgg <joseph.brightman@gmail.com>
2025-11-05 19:50:57 +0000

    removing magic numbers trying our constants in util functions

---
+++ app/src/main/java/uk/co/zlurgg/thedayto/journal/ui/overview/util/TimeConstants.kt
@@ -0,0 +1,19 @@
+package uk.co.zlurgg.thedayto.journal.ui.overview.util
+
+/**
+ * Time-related constants for overview screen.
+ */
+object TimeConstants {
+    // Time ranges for greetings
+    const val NIGHT_START = 0
+    const val NIGHT_END = 4
+    const val MORNING_START = 5
+    const val MORNING_END = 11
+    const val AFTERNOON_START = 12
+    const val AFTERNOON_END = 16
+    const val EVENING_START = 17
+    const val EVENING_END = 20
+
+    // Delays
+    const val LOADING_DEBOUNCE_MS = 150L
+}
