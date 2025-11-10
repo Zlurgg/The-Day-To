c303448cf9078b444ba31430190ec5c49ed54e4b
Zlurgg <joseph.brightman@gmail.com>
2025-11-10 17:21:14 +0000

    lets remove onaction and in that case we don't need StatsAction.kt all we do is load the stats on this page

--- app/src/main/java/uk/co/zlurgg/thedayto/journal/ui/stats/state/StatsAction.kt
+++
@@ -1,9 +0,0 @@
-package uk.co.zlurgg.thedayto.journal.ui.stats.state
-
-/**
- * User actions on the Stats screen
- */
-sealed interface StatsAction {
-    data object Refresh : StatsAction
-    data object NavigateBack : StatsAction
-}
