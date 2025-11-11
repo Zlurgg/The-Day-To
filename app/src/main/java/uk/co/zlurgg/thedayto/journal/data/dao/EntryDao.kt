2dd102371464b65c095a026b442c4f630b3efd21
Zlurgg <joseph.brightman@gmail.com>
2025-11-11 12:03:01 +0000

    new queries for use later in dao

--- app/src/main/java/uk/co/zlurgg/thedayto/journal/data/dao/EntryDao.kt
+++ app/src/main/java/uk/co/zlurgg/thedayto/journal/data/dao/EntryDao.kt
@@ -8,6 +8,7 @@ import androidx.room.Query
 import androidx.room.Update
 import kotlinx.coroutines.flow.Flow
 import uk.co.zlurgg.thedayto.journal.data.model.EntryEntity
+import uk.co.zlurgg.thedayto.journal.data.model.EntryWithMoodColorEntity
 
 @Dao
 interface EntryDao {
@@ -15,11 +16,38 @@ interface EntryDao {
     @Query("SELECT * FROM entry")
     fun getEntries(): Flow<List<EntryEntity>>
 
+    @Query("""
+        SELECT e.id, e.moodColorId, e.content, e.dateStamp,
+               mc.mood as moodName, mc.color as moodColor
+        FROM entry e
+        INNER JOIN mood_color mc ON e.moodColorId = mc.id
+        ORDER BY e.dateStamp DESC
+    """)
+    fun getEntriesWithMoodColors(): Flow<List<EntryWithMoodColorEntity>>
+
     @Query("SELECT * FROM entry WHERE id = :id")
     suspend fun getEntryById(id: Int): EntryEntity?
 
+    @Query("""
+        SELECT e.id, e.moodColorId, e.content, e.dateStamp,
+               mc.mood as moodName, mc.color as moodColor
+        FROM entry e
+        INNER JOIN mood_color mc ON e.moodColorId = mc.id
+        WHERE e.id = :id
+    """)
+    suspend fun getEntryWithMoodColorById(id: Int): EntryWithMoodColorEntity?
+
     @Query("SELECT * FROM entry WHERE dateStamp = :date")
     suspend fun getEntryByDate(date: Long): EntryEntity?
+
+    @Query("""
+        SELECT e.id, e.moodColorId, e.content, e.dateStamp,
+               mc.mood as moodName, mc.color as moodColor
+        FROM entry e
+        INNER JOIN mood_color mc ON e.moodColorId = mc.id
+        WHERE e.dateStamp = :date
+    """)
+    suspend fun getEntryWithMoodColorByDate(date: Long): EntryWithMoodColorEntity?
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertEntry(entry: EntryEntity)
