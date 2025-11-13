224b8852574600e09e9411825f64c70e72d62604
Zlurgg <joseph.brightman@gmail.com>
2025-11-13 15:50:06 +0000

    new usecases for first time users flow

--- app/src/main/java/uk/co/zlurgg/thedayto/journal/domain/usecases/overview/OverviewUseCases.kt
+++ app/src/main/java/uk/co/zlurgg/thedayto/journal/domain/usecases/overview/OverviewUseCases.kt
@@ -11,6 +11,8 @@ data class OverviewUseCases(
     val updateEntryUseCase: UpdateEntryUseCase,
     val checkEntryReminderShownToday: CheckEntryReminderShownTodayUseCase,
     val markEntryReminderShownToday: MarkEntryReminderShownTodayUseCase,
+    val checkFirstLaunch: CheckFirstLaunchUseCase,
+    val markFirstLaunchComplete: MarkFirstLaunchCompleteUseCase,
     val getNotificationSettings: uk.co.zlurgg.thedayto.core.domain.usecases.notifications.GetNotificationSettingsUseCase,
     val saveNotificationSettings: uk.co.zlurgg.thedayto.core.domain.usecases.notifications.SaveNotificationSettingsUseCase,
     val checkNotificationPermission: uk.co.zlurgg.thedayto.core.domain.usecases.notifications.CheckNotificationPermissionUseCase,
