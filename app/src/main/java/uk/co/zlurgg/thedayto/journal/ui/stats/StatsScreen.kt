e946257204a7f4de5c1d0cfea3048335ee0b82a7
Zlurgg <joseph.brightman@gmail.com>
2025-11-17 13:59:40 +0000

    hardcoded string extracted

--- app/src/main/java/uk/co/zlurgg/thedayto/journal/ui/stats/StatsScreen.kt
+++ app/src/main/java/uk/co/zlurgg/thedayto/journal/ui/stats/StatsScreen.kt
@@ -26,11 +26,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import org.koin.androidx.compose.koinViewModel
+import uk.co.zlurgg.thedayto.R
 import uk.co.zlurgg.thedayto.core.ui.theme.TheDayToTheme
 import uk.co.zlurgg.thedayto.core.ui.theme.paddingMedium
 import uk.co.zlurgg.thedayto.journal.ui.stats.components.MonthlyBreakdownCard
@@ -68,7 +70,7 @@ private fun StatsScreen(
     Scaffold(
         topBar = {
             TopAppBar(
-                title = { Text("Statistics") },
+                title = { Text(stringResource(R.string.statistics)) },
                 navigationIcon = {
                     IconButton(onClick = onNavigateBack) {
                         Icon(
