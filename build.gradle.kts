86d94fe52d9ff2899111aab4e8e52e80eeba6927
Zlurgg <joseph.brightman@gmail.com>
2026-01-12 17:55:13 +0000

    build: Add Detekt with formatting module (ktlint wrapper)

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,4 +15,5 @@ plugins {
     alias(libs.plugins.google.services) apply false
     alias(libs.plugins.kotlin.parcelize) apply false
     alias(libs.plugins.kotlin.serialization) apply false
+    alias(libs.plugins.detekt) apply false
 }
