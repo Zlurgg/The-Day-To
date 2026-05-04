44bc55fa652311793850b0548631291371f91cec
Zlurgg <joseph.brightman@gmail.com>
2026-05-04 15:51:47 +0100

    chore(release): bump version to 2.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         applicationId = "uk.co.zlurgg.thedayto"
         minSdk = 27
         targetSdk = 36
-        versionCode = 18
-        versionName = "2.1.1"
+        versionCode = 19
+        versionName = "2.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
